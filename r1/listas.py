0f5e505507e5634adf18e2f3bf4f43cfea582c35
valgarc12 <valeria.garciajimenez240@gmail.com>
2025-03-23 18:50:52 -0600

    Add files via upload

---
+++ r1/listas.py
@@ -0,0 +1,67 @@
+
+import random as r
+import pandas as pd
+abecedario=[ 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z']
+papelerias = ['Xochimilco', 'Cuemanco', 'Coapa', 'Milpa Alta', 'CU', 'Zócalo',
+              'Narvarte', 'Santa Fé', 'Polanco', 'Centro']
+lineas = ['Cuadernos', 'Libretas', 'Lápices', 'Plumones', 'Borradores', 'Sacapuntas',
+          'Laptops', 'Tablets', 'Mochilas', 'Bolsas', 'Cajas', 'Pegamento', 'Tijeras',
+          'Monitores', 'Teclados', 'Mouse', 'Audífonos', 'Cables', 'Cargadores', 'Baterías',
+          'Pc', 'Uniformes', 'Pinturas', 'Pinceles', 'Papel', 'Cartulinas']
+
+#fecha = "16/03/2025"
+#sucursal = r.choice(papelerias)
+#producto = r.choice(lineas)
+#clave_producto = r.choice(abecedario) + r.choice(abecedario) + r.choice(abecedario) + "-" + str(r.randint(1,9)) + str(r.randint(1,9)) + str(r.randint(1,9))
+#precio = r.random() * r.randint(100,5000) 
+#cantidad_vendida = r.randint(1,20)
+#total_ticket = precio * cantidad_vendida 
+#print(f"Fecha: {fecha}")
+#print(f"Sucursal: {sucursal}")
+#print(f"Producto: {producto}")
+#print(f"Clave de Producto: {clave_producto}")
+#print(f"Precio: {precio}")
+#print(f"Cantidad Vendida: {cantidad_vendida})
+
+fechas = []
+sucursales = []
+productos = []
+claves_producto = []
+precios = []
+cantidades_vendidas = []
+totales_ticket = []
+
+for i in range (1,1001):
+ #Información a trabajar.
+ fecha = "16/03/2025"
+ sucursal = r.choice(papelerias)
+ producto = r.choice(lineas)
+ clave_producto = r.choice(abecedario) + r.choice(abecedario) + r.choice(abecedario) + "-" + str(r.randint(1,9)) + str(r.randint(1,9)) + str(r.randint(1,9))
+ precio = r.random() * r.randint(100,5000) 
+ cantidad_vendida = r.randint(1,20)
+ total_ticket = precio * cantidad_vendida
+
+ 
+ #Agregamos a listas usando .append
+ fechas.append(fecha)
+ sucursales.append(sucursal)
+ productos.append(producto)
+ claves_producto.append(clave_producto)
+ precios.append(precio)
+ cantidades_vendidas.append(cantidad_vendida)
+ totales_ticket.append(total_ticket)
+ 
+dc_1 = {
+    "Fecha": fechas,
+    "Sucursal" : sucursales,
+    "Producto" : productos,
+    "Clave_producto" : claves_producto, 
+    "Precio": precios,
+    "Cantidad_vendida" : cantidades_vendidas,
+    "Total" : totales_ticket    
+} 
+df_ventas = pd.DataFrame(dc_1)
+print(df_ventas)
+
